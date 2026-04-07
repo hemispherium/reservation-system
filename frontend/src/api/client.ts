a2b0213b027ac870daa3da0b9a636c5be0d44d89
Yu Nakamura <nkmr357@gmail.com>
2026-04-07 16:08:55 +0900

    fix

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const client = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8002/api',
-  withCredentials: true,
   headers: {
     Accept: 'application/json',
   },
