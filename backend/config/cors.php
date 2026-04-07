dd80f8bbfebd2e98eae62f1f73637580ae54594e
Yu Nakamura <nkmr357@gmail.com>
2026-04-07 15:23:33 +0900

    fix cors setting

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174'],
+    'allowed_origins' => array_filter(array_merge(
+        ['http://localhost:5173', 'http://localhost:5174'],
+        env('CORS_ALLOWED_ORIGINS') ? explode(',', env('CORS_ALLOWED_ORIGINS')) : []
+    )),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
